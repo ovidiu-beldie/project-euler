7df34402e18d0e8623da4b2486ef272e092537a4
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-01-29 15:03:45 +0100

    Problem 5.

---
+++ src/euler/p5.clj
@@ -0,0 +1,12 @@
+;2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+;What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+(ns euler.p5
+	(:use [euler.p3 :only (factorize)]
+				[incanter.core :only (pow)]))
+
+(defn lcd [coll]
+	(let [facts (map factorize (drop-while #(< % 2) coll))
+				histo (map frequencies facts)
+				highest-order-facts (apply merge-with max histo)]
+		(long (reduce * (map #(pow (key %) (val %)) highest-order-facts)))))

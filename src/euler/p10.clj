6913b940737823c040237619729614d771cd7ede
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-02-01 01:00:24 +0100

    P10: tabs to spaces

--- src/euler/p10.clj
+++ src/euler/p10.clj
@@ -2,7 +2,7 @@
 ;Find the sum of all the primes below two million.
 
 (ns euler.p10
-	(:use [euler.p3 :only (primes)]))
+  (:use [euler.p3 :only (primes)]))
 
 ;;; Using division testing
 (defn sum [lim]
@@ -16,10 +16,10 @@
 
 (defn index-of [n]
   "Returns the index of n in the sieve
-	which is [(n+1)/2]-1"
-	(if (odd? n)
-		(dec (/ (inc n) 2))
-		0))  ; the only even prime is 2 and it's the first in the seq
+  which is [(n+1)/2]-1"
+  (if (odd? n)
+    (dec (/ (inc n) 2))
+    0))  ; the only even prime is 2 and it's the first in the seq
 
 (defn find-next-prime [n s s-count]
   "Find the first prime superior to n in the sieve s"
@@ -33,24 +33,24 @@
 (defn mark-multiples [n si si-count]
   "Marks all the multiples of n in the sieve si
    which has si-count elements"
-	(let [index-n (index-of n)]
-		(loop [i (+ index-n n), s si]
-			(if (>= i si-count)
-				s
+  (let [index-n (index-of n)]
+    (loop [i (+ index-n n), s si]
+      (if (>= i si-count)
+        s
         (let [si
                 (if (val (first (s i)))
-				          (let [marked {(key (first (s i))), false}]
+                  (let [marked {(key (first (s i))), false}]
                     (assoc s i marked))
                 s)]
-					(recur (+ i n), si))))))
+          (recur (+ i n), si))))))
 
 (defn sieve [lim]
   "Sieve up to lim (e.g. 2000000)"
-	(let [nbs (cons 2 (filter odd? (range 2 (inc lim))))
-				si (vec (for [x nbs] {x true}))
-			  si-count (count si)]
-		(loop [s si, old-n 2]
-			(let [n (find-next-prime old-n s si-count)]
-				(if (nil? n)
-					(map #(key (first %)) (filter #(val (first %)) s))
-					(recur (mark-multiples n s si-count) n))))))
+  (let [nbs (cons 2 (filter odd? (range 2 (inc lim))))
+        si (vec (for [x nbs] {x true}))
+        si-count (count si)]
+    (loop [s si, old-n 2]
+      (let [n (find-next-prime old-n s si-count)]
+        (if (nil? n)
+          (map #(key (first %)) (filter #(val (first %)) s))
+          (recur (mark-multiples n s si-count) n))))))

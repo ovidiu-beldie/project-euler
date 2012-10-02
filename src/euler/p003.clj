09cd278d709f3a391b37748b5e161126dc3b8fb8
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-10-02 17:09:23 +0200

    Updated P3 and renamed file to p003.clj

--- src/euler/p003.clj
+++ src/euler/p003.clj
@@ -1,17 +1,26 @@
 ;The prime factors of 13195 are 5, 7, 13 and 29.
-;What is the largest prime factor of the number 600851475143 ?
+;What is the largest prime factor of the number 600851475143?
 
-(ns euler.p3
-  (:use [incanter.core :only (sqrt)]))
+(defn lpf
+  [num]
+  (let [div? (fn [n d] (zero? (mod n d)))] ;divisible predicate
+    (loop [n num, d 2]
+      (cond
+       (> d (Math/sqrt n)) n
+       (div? n d) (recur (/ n d) 2)
+       :else (recur n (inc d))))))
+
+
+;;; Earlier version (around march 2012)
 
 (defn prime? [n inf-primes]
-  "Returns true if n is a prime. 
+  "Returns true if n is a prime.
    inf-primes is the sequence of primes inferior to n"
   (loop [ps inf-primes]
     (let [p (first ps)]
-      (if (> p (sqrt n))
+      (if (> p (Math/sqrt n))
         true
-        (if (zero? (rem n p)) 
+        (if (zero? (rem n p))
           false
           (recur (next ps)))))))
 
@@ -21,7 +30,7 @@
     (if (prime? x primes)
       (conj primes x)
       (recur (inc x)))))
-  
+
 (defn primes []
   "Infinite sequence of primes"
   (map last (iterate next-prime [2])))
@@ -37,6 +46,3 @@
           (if (= f x)
             (conj facts f)
             (recur (/ x f) (conj facts f))))))))
-    
-
-      

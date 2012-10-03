416ec5de99d03e071a3fe1237add9284dd7c752c
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-10-03 14:20:42 +0200

    Updated and renamed P1

--- src/euler/p001.clj
+++ src/euler/p001.clj
@@ -1,9 +1,13 @@
-;If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+;If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+;The sum of these multiples is 23.
 ;Find the sum of all the multiples of 3 or 5 below 1000.
 
-(ns euler.p1)
+(defn p1
+  ([] (p1 3 5 1000))
+  ([x y lim]
+     (reduce + (into (apply hash-set (range 3 1000 3)) (apply hash-set (range 5 1000 5))))))
 
-(defn p1 [x y lim]
+(defn p1-1 [x y lim]
   "For this particular problem x=3, y=5, lim=1000"
   (let [make-seq (fn [a] (take-while #(< % lim) (iterate #(+ % a) a)))
         not-multiple? (fn [a b] (not (zero? (rem a b))))]

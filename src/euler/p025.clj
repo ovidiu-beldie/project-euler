d82a34bb245d393c6cba67bcf9ca872b22966c3e
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-12-04 15:33:34 +0100

    Provided new solution for P25 and changed file's name

--- src/euler/p025.clj
+++ src/euler/p025.clj
@@ -21,15 +21,25 @@
 
 ;What is the first term in the Fibonacci sequence to contain 1000 digits?
 
+(require '[euler.fibo :as f])
 
-(ns euler.p25)
+(defn p25
+  "Solution to P25"
+  ([] (p25 1000))
+  ([n]
+     (->> (map vector (range) (f/fibo))
+          (drop-while #(< (count (str (last %))) n))
+          (ffirst))))
 
-(defn fibo []
+
+;;; Earlier implementation (march 2012)
+
+(defn fibo-old []
   ; Christophe Grand's solution
   (map first (iterate (fn [[a b]] [(bigdec b) (+ a b)]) [0 1])))
 
-(defn p25 [len]
-  (let [index-fibo (map vector (fibo) (range))
+(defn p25-old [len]
+  (let [index-fibo (map vector (fibo-old) (range))
         elem-before (last (take-while #(< (count (.toString (first %))) len) index-fibo))]
     (prn (inc (second elem-before)))))
 
